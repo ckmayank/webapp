f88138bf22a6f21c3525e11c8ad0b1b165dbaa30
ckmayank <141540005+ckmayank@users.noreply.github.com>
2024-04-04 22:17:19 +0530

    Add files via upload

---
+++ webapp/website.py
@@ -0,0 +1,67 @@
+import os
+import cv2
+import numpy as np
+import easyocr
+from autocorrect import Speller
+from flask import Flask, render_template, request, send_file, flash
+from PIL import Image
+
+app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = 'uploads'
+
+spell = Speller(lang='en')
+reader = easyocr.Reader(['en'], gpu=False)
+
+def ocr_process(image_path):
+    if not os.path.exists(image_path):
+        return '', 404
+
+    nparr = np.fromfile(image_path, np.uint8)
+    image_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+    processed_image = process_frame(image_np)
+    text_results = reader.readtext(processed_image)
+
+    corrected_text = ''
+    for result in text_results:
+        corrected_text += spell(result[1]) + '\n'
+
+    return {'detected_text': corrected_text}
+
+def process_frame(frame):
+    pil_image = Image.fromarray(frame)
+    gray_image = pil_image.convert('L')
+    opencv_image = np.array(gray_image)
+
+    return opencv_image
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'file' not in request.files:
+        return '', 400
+
+    file = request.files['file']
+    if not file:
+        return '', 400
+
+    filename = 'capture.png'
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(filepath)
+
+    result = ocr_process(filepath)
+
+    if not result:
+        return '', 400
+
+    os.remove(filepath)
+
+    return result
+
+if __name__ == '__main__':
+
+    app.run(debug=False)
